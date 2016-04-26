bee0db07c4399c006d8be0c6b68a803eea0dedc7
Chad Waters <authorblues@gmail.com>
2016-04-26 03:36:47 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,348 @@
+var ORIGINAL_ROM = null;
+var BASEURL = window.location.origin + window.location.pathname;
+
+var EN_US = false;
+
+function doRandomize(buffer, seed)
+{
+	try
+	{
+		var result = randomizeROM(buffer, seed);
+		var url = BASEURL + '#!/' + result.seed + '/' + result.preset;
+		$('#link-text').val(url);
+	//	saveAs(new Blob([result.buffer], {type: "octet/stream"}), 'mtpo-' + result.seed + result.type);
+	}
+	catch (e)
+	{
+		$('#modal-error-win #modal-error-text').text(e.name + ': ' + e.message);
+		$('#modal-error-win #modal-error-list').empty();
+
+		if (e instanceof ValidationError)
+			for (var i = 0; i < e.errors.length; ++i)
+				$('#modal-error-win #modal-error-list').append($('<li>').text(e.errors[i]).addClass('mono'));
+
+		$('#modal-error-win').modal('show');
+		throw e;
+	}
+}
+
+$('#generate-randomized-rom').click(function(e)
+{
+	if (!ORIGINAL_ROM) return;
+
+	// maybe this will be a NaN?
+	var seed = parseInt($('#custom-seed').val(), 16);
+
+	if (ORIGINAL_ROM === true)
+	{
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'mtpo.nes', true);
+		xhr.responseType = 'arraybuffer';
+
+		xhr.onload = function(e){ doRandomize(xhr.response, seed); }
+		xhr.send();
+	}
+	else
+	{
+		var reader = new FileReader();
+		reader.onloadend = function(e){ doRandomize(reader.result, seed); };
+		reader.readAsArrayBuffer(ORIGINAL_ROM);
+	}
+});
+
+$('#generate-param-rom').click(function(e)
+{
+	// maybe this will be a NaN?
+	var seed = parseInt($('#custom-seed').val(), 16);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'mtpo.nes', true);
+	xhr.responseType = 'arraybuffer';
+
+	xhr.onload = function(e){ doRandomize(xhr.response, seed); }
+	xhr.send();
+});
+
+function _validateRandomizer(buffer, maxiter, iter, errors)
+{
+	for (var i = 0; i < 100 && iter > 0; ++i, --iter)
+	{
+		var copy = new ArrayBuffer(buffer.byteLength);
+		new Uint8Array(copy).set(new Uint8Array(buffer));
+		try { randomizeROM(copy); } catch (e) { ++errors; }
+	}
+
+	if (iter) setTimeout(_validateRandomizer.bind(this, buffer, maxiter, iter, errors), 100);
+	else console.log('Validation complete: ' + errors + ' errors (' + Math.round(errors*100/maxiter) + '%)'); // FIXME
+}
+
+function validateRandomizer(iter)
+{
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'mtpo.nes', true);
+	xhr.responseType = 'arraybuffer';
+
+	iter = iter || 10000;
+	xhr.onload = function(e)
+	{
+		console.log('Starting ' + iter + ' iterations of the randomizer...');
+		_validateRandomizer(xhr.response, iter, iter, 0);
+	};
+	xhr.send();
+}
+
+function getMD5(file, callback)
+{
+	var w = new Worker('js/md5.worker.js');
+	w.onmessage = function(e){ callback(e.data); };
+	w.postMessage(file);
+}
+
+$('#select-original-rom').click(
+	function(e){ $('#original-rom').click(); });
+
+$('#original-rom').change(function(e)
+{
+	getMD5(e.target.files[0], function(x)
+		{ checkRomResult(x in ORIGINAL_MD5, e.target.files[0]); });
+});
+
+$('form').submit(function(e)
+{
+	e.preventDefault();
+	return false;
+});
+
+$('#authorblues').click(function(e)
+{
+	checkRomResult(true, true);
+	$('#select-original-rom').prop('disabled', true);
+});
+
+function cleanCustomSeed(seed)
+{ return seed.replace(/[^a-fA-F0-9]+/g, '').substr(0, 8); }
+
+$('#custom-seed').bind("keypress paste", function(e)
+{
+	var self = $(this);
+	setTimeout(function(){ self.val(cleanCustomSeed(self.val())); }, 1);
+});
+
+function checkRomResult(valid, file)
+{
+	$('#original-rom-result').removeClass('glyphicon-question-sign');
+	$('#original-rom-result').toggleClass('glyphicon-ok', valid);
+	$('#original-rom-result').toggleClass('glyphicon-remove', !valid);
+
+	$('#generate-randomized-rom').prop('disabled', !valid);
+
+	if (valid) ORIGINAL_ROM = file;
+}
+
+function checkHash()
+{
+	if (!location.hash || location.hash.indexOf("#!/") !== 0) return;
+	var parts = location.hash.split('/').slice(1);
+
+	var seed = cleanCustomSeed(parts[0]);
+	if (parts.length > 0) $('#custom-seed').val(seed);
+
+	var given = parts.length > 1 ? parts[1] : '0';
+	if (given[0] == 'x')
+	{
+		$('#preset').val(0);
+		setRandomizerSettings(given.substr(1));
+	}
+	else
+	{ $('#preset').val(+parts[1]); updatePreset(); }
+
+	if (parts.length > 1)
+	{
+		$('#modal-download-win .modal-body .seed').text(seed);
+		$('#modal-download-win .modal-body .preset').text(getPresetName());
+		$('#modal-download-win').modal('show');
+	}
+}
+
+function deepClone(obj)
+{
+	if (obj.constructor == Array)
+	{
+		var x = [], i;
+		for (i = 0; i < obj.length; ++i)
+			x.push(deepClone(obj[i]));
+		return x;
+	}
+	else if (obj.constructor == Object)
+	{
+		var x = {};
+		for (var k in obj)
+			if (obj.hasOwnProperty(k))
+				x[k] = deepClone(obj[k]);
+		return x;
+	}
+	else return obj;
+}
+
+window.onhashchange = checkHash;
+checkHash();
+
+Math.sign = Math.sign || function(x)
+{
+	x = +x;
+	if (x > 0) return  1;
+	if (x < 0) return -1;
+	return 0;
+}
+
+Uint8Array.prototype.slice = Uint8Array.prototype.slice || function(start, end)
+{
+	var src = this.subarray(start, end);
+	var dst = new Uint8Array(src.byteLength);
+	dst.set(src); return dst;
+}
+
+Uint8Array.prototype.writeBytes = function(b, addr, val)
+{ var _b = b; for (; b--; val >>= 8) this[addr++] = val & 0xFF; return _b; }
+
+//Uint8Array.prototype.readBytes = function(b, addr)
+//{ var x = 0, s = 0; for (; b--; s += 8, addr++) x |= (this[addr] & 0xFF) << s; return x; }
+
+function bitsToHex(_arr)
+{
+	var arr = _arr.slice(0);
+
+	var h = '', x, i;
+	while (arr.length)
+	{
+		var z = arr.splice(0, 4);
+		for (x = 0, i = 0; i < z.length; ++i)
+			x |= (z[i] ? 1 : 0) << i;
+		h += x.toString(16);
+	}
+
+	return h;
+}
+
+function hexToBits(x)
+{
+	for (var a = [], i = 0; i < x.length; ++i)
+	{
+		var v = parseInt(x[i], 16);
+		for (var j = 0; j < 4; v >>= 1, ++j) a.push(v & 1);
+	}
+	return a;
+}
+
+function Random(seed)
+{ this.seed = Math.floor(seed || (Math.random() * 0xFFFFFFFF)) % 0xFFFFFFFF; }
+
+Random.prototype.clone = function()
+{ return new Random(this.seed); }
+
+Random.prototype.pull = function(n)
+{ while (n--) this.next(); }
+
+Random.prototype.next = function(z)
+{ return this.seed = ((214013 * this.seed + 2531011) & 0x7fffffff) >> 16; }
+
+Random.prototype.nextFloat = function()
+{ return this.next() / 0x7fff; }
+
+Random.prototype.flipCoin = function(x)
+{ return this.nextFloat() < x; }
+
+// Box-Muller transform, converts uniform distribution to normal distribution
+// depends on uniformity of nextFloat(), which I'm not confident of
+Random.prototype.nextGaussian = function()
+{
+	var u = this.nextFloat(), v = this.nextFloat();
+	return Math.sqrt(-2 * Math.log(u)) * Math.cos(2 * Math.PI * v);
+}
+
+Random.prototype.nextInt = function(z)
+{ return (this.nextFloat() * z)|0; }
+
+Random.prototype.nextIntRange = function(a, b)
+{ return a + this.nextInt(b - a); }
+
+Random.prototype.from = function(arr)
+{ return arr[this.nextInt(arr.length)]; }
+
+Random.prototype.draw = function(arr)
+{
+	var which = this.nextInt(arr.length);
+	return arr.splice(which, 1)[0];
+}
+
+Array.prototype.shuffle = function(random)
+{
+	if (!random) random = new Random();
+	for (var t, i = 1, j; i < this.length; ++i)
+	{
+		j = random.nextInt(i);
+		t = this[j]; this[j] = this[i]; this[i] = t;
+	}
+
+	return this;
+}
+
+Array.prototype.contains = function(x)
+{ return this.indexOf(x) != -1; }
+
+Number.prototype.toHex = function(n, p)
+{
+	var hex = this.toString(16);
+	while (hex.length < n) hex = '0' + hex;
+	return (p != null ? p : '') + hex;
+}
+
+Number.prototype.toPrintHex = function(n)
+{ return '0x' + this.toHex(n).toUpperCase(); }
+
+function ROMLogger(rom)
+{ this.rom = rom; }
+
+ROMLogger.prototype.start = function()
+{
+	this.orig = new Uint8Array(this.rom.byteLength);
+	this.orig.set(this.rom);
+}
+
+ROMLogger.prototype.print = function()
+{
+	for (var i = 0; i < this.rom.length; ++i)
+	{
+		if (this.rom[i] !== this.orig[i]) continue;
+		console.log(i.toHex(6, '0x') + ' - ' + this.orig[i].toHex(2) + '->' + this.rom[i].toHex(2));
+	}
+}
+
+$(document).ready(function()
+{
+	// use RANDOMIZER if language is set to en_US
+	var language = window.navigator.userLanguage || window.navigator.language || "";
+	EN_US = (language.indexOf('US') != -1);
+
+	if (!EN_US) document.title = document.title.replace('ize', 'ise');
+	$('[data-en_gb]').each(function()
+	{
+		var s = $(this);
+		s.text(EN_US ? s.attr('data-en_us') : s.attr('data-en_gb'));
+	});
+});
+
+var TESTERS =
+{
+
+}
+
+$('#tester-list').html(
+	$.map(TESTERS, function(twitch, name)
+	{
+		var str = twitch ? '<a href="http://twitch.tv/' + twitch + '">' + name + '</a>' : name;
+		return '<span class="tester-' + name + '">' + str + '</span>';
+	}).shuffle().join(', ')
+);
+
+$('.version-number').text(VERSION_STRING);
---
+++ js/randomizer.js
@@ -0,0 +1,138 @@
+var VERSION_STRING = 'v0.1';
+
+function randomizeROM(buffer, seed)
+{
+	var ext = '.nes';
+
+	var random = new Random(seed);
+	var vseed = random.seed.toHex(8);
+
+	$('#custom-seed').val('');
+	$('#used-seed').text(vseed);
+
+	var rom = new Uint8Array(buffer);
+
+	if ($('#randomize_roundlength').is(':checked'))
+		randomizeRoundLength(random, rom);
+
+	if ($('#randomize_hearts').is(':checked'))
+		randomizeHearts(random, rom);
+
+	if ($('#glassjoe_patterns' ).is(':checked')) randomizeGlassJoePatterns(random, rom);
+	if ($('#glassjoe_damage'   ).is(':checked')) randomizeGlassJoeDamage(random, rom);
+	if ($('#glassjoe_threshold').is(':checked')) randomizeGlassJoeThreshold(random, rom);
+
+	// update the location.hash
+	var preset = +$('#preset').val();
+	if (!preset) preset = 'x' + getRandomizerSettings();
+
+	// return the modified buffer
+	return { seed: vseed, preset: preset, buffer: rom.buffer, type: ext || '.nes' };
+}
+
+function randomizeRoundLength(random, rom)
+{
+	rom[0x0E7D2] = 1 + random.nextInt(5);
+	rom[0x0E80E] = rom[0x0E7D2] + 1;
+}
+
+function randomizeHearts(random, rom)
+{
+	// hearts lost when opponent lands punch on Mac
+	rom[0x0E154] = random.nextIntRange(0x06, 0x10);
+
+	// hearts lost when Mac blocks a punch
+	rom[0x0E0E2] = random.nextIntRange(0x01, 0x06);
+
+	// Glass Joe
+	rom[0x0132C] = 1 + random.nextInt(20); // start round 1
+	rom[0x0132F] = 1 + random.nextInt(20); // start round 2
+	rom[0x01330] = 1 + random.nextInt(20); // start round 3
+	rom[0x0132D] = 1 + random.nextInt(15); // after running out and dodging
+	rom[0x0132E] = 1 + random.nextInt( 9); // after getting up from KO
+}
+
+function randomizeGlassJoePatterns(random, rom)
+{
+	rom[0x01434] = random.nextIntRange(0x0A, 0xB6); // timing of round 1 charge attack
+	rom[0x01450] = random.nextIntRange(0x0A, 0xB6); // timing of round 2 charge attack
+	rom[0x0145F] = random.nextIntRange(0x0A, 0xB6); // timing of round 3 charge attack
+
+	rom[0x0146C] = random.from([0x73, 0x78]); // wait until charge in round 1 before punching (73=y, 78=n)
+	rom[0x01476] = random.from([0x80, 0x50]); // can dodge? (80=y, 50=n)
+
+	// number of punches before being awarded stars 1st and subsequent times
+	rom[0x01336] = 3 + Math.min(Math.floor(Math.abs(random.nextGaussian() * 10)), 100);
+	rom[0x01337] = 5 + Math.min(Math.floor(Math.abs(random.nextGaussian() * 10)), 100);
+
+	// downtime values
+	for (var i = 0; i < 8; ++i) rom[0x01366+i] = random.nextIntRange(0x9B, 0xA3);
+
+	// special scenario downtime values (XY, X = count, Y = health when up)
+	rom[0x013D7] = (random.nextIntRange(0x01, 0x0A) << 4) | random.nextIntRange(0x01, 0x0D);
+
+	rom[0x01083] = 1 + random.nextInt(0xFF); // GJ waiting time before charging
+	rom[0x01387] = random.nextInt(0x08); // # of hits when stunned (dodge hook)
+	rom[0x00C1B] = random.nextInt(0x08); // # of hits when stunned (block)
+	rom[0x00C25] = random.nextInt(0x08); // # of hits when stunned (jab and ducking)
+
+	rom[0x17A87] = random.from([0x04, 0x05]); // situation 1 (04=blocks, 05=punch lands)
+	rom[0x17AD3] = random.from([0x04, 0x05]); // situation 2 (04=blocks, 05=punch lands)
+	rom[0x17B73] = random.from([0x04, 0x05]); // situation 3 (04=blocks, 05=punch lands)
+
+	rom[0x01473] = random.nextIntRange(0x05, 0x61); // delay after blocking GJ's hook
+	rom[0x00C1A] = random.nextIntRange(0x05, 0x31); // delay after blocking GJ's jab
+
+	// chance of blocking in several scenarios
+	var blockchance = 0.1 + random.nextFloat() * 0.7;
+	console.log('glass joe block chance', blockchance);
+
+	rom[0x00C6D] = random.flipCoin(blockchance) ? 0x08 : 0x00;
+	rom[0x012B5] = random.flipCoin(blockchance) ? 0x08 : 0x00;
+
+	for (var i = 0x1C838; i <= 0x1C83F; ++i)
+		rom[i] = random.flipCoin(blockchance) ? 0x08 : 0x00;
+
+	for (var i = 0x012BD; i <= 0x012BE; ++i)
+		rom[i] = random.flipCoin(blockchance) ? 0x08 : 0x00;
+
+	for (var i = 0x012BB; i <= 0x012BC; ++i)
+		rom[i] = random.flipCoin(blockchance) ? 0x08 : 0x00;
+
+	// round 1 patterns
+	rom.set(random.from([[0x80, 0xFF], [0x04, 0x95], [0x04, 0x96]]), 0x01484);
+	rom[0x0148A] = random.nextIntRange(0x05, 0x46);
+	rom[0x0148E] = random.from([0x95, 0x96]);
+	rom[0x01491] = random.from([0x95, 0x96]);
+	rom[0x01494] = random.from([0x95, 0x96]);
+	rom[0x01498] = random.from([0x95, 0x96]);
+	rom[0x0149B] = random.from([0x95, 0x96]);
+	rom[0x0149E] = random.from([0x95, 0x96]);
+
+	// round 2/3 patterns
+	rom[0x014A7] = random.from([0x95, 0x96, 0x99]);
+	rom[0x014A9] = random.from([0x95, 0x96, 0x99]);
+	rom[0x014AD] = random.nextIntRange(0x05, 0x26);
+	rom[0x014B1] = random.from([0x95, 0x96]);
+	rom[0x014B4] = random.from([0x95, 0x96]);
+	rom[0x014B7] = random.from([0x95, 0x96]);
+	rom[0x014BB] = random.from([0x95, 0x96]);
+	rom[0x014BE] = random.from([0x95, 0x96]);
+	rom[0x014C1] = random.from([0x95, 0x96]);
+}
+
+function randomizeGlassJoeDamage(random, rom)
+{
+	rom[0x01359] = random.nextIntRange(0x00, 0x21); // damage to GJ from uppercut
+	rom[0x17C1F] = random.nextIntRange(0x06, 0x20); // damage to opponent from uppercut (high, low)
+	rom[0x00DB8] = random.nextIntRange(0x00, Math.min(0x09, rom[0x17C1F]));
+
+	// health when GJ gets back up in each of 7 different situations
+	for (var i = 0; i < 7; ++i) rom[0x013E0+i] = random.nextIntRange(0x03, 0x0D);
+}
+
+function randomizeGlassJoeThreshold(random, rom)
+{
+	rom[0x0134F] = random.nextInt( 2); // points in 10,000s
+	rom[0x01350] = random.nextInt(10); // points in  1,000s
+}
